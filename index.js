63d9ef7101abc6dd47f8dc0571dfd3c1cff5166c
Carlos <miklo.concepcion@gmail.com>
2019-12-11 19:48:23 -0600

    Add new environment variables

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express = require("express")
+const express = require("express");
+// const dotenv = require("dotenv");
 
-const app = express()
-const host = "0.0.0.0"
-const port = 8080
+// dotenv.config();
+
+const app = express();
+const host = "0.0.0.0";
+const port = 8080;
 
 app.use((req, res, next) => {
-	console.log(`[${new Date().toLocaleString()}] ${req.ip} ${req.method} ${req.url}`)
-	next()
-})
+  console.log(
+    `[${new Date().toLocaleString()}] ${req.ip} ${req.method} ${req.url}`
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
-	res.json({
-		message: "Welcome to our API",
-	})
-})
+  res.json({
+    message: "Welcome to our API",
+    cohort: process.env.LAMBDA_COHORT,
+    secret: process.env.SUPER_SECRET_API_KEY
+  });
+});
 
 app.listen(port, host, () => {
-	console.log(`Running at http://${host}:${port}`)
-})
+  console.log(`Running at http://${host}:${port}`);
+});
