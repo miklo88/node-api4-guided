dabae2af59b9f6b3b9b657af70d2bfa0b662a241
Carlos <miklo.concepcion@gmail.com>
2019-12-11 21:56:08 -0600

    updating project.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 8080;
 
 app.use((req, res, next) => {
+  const actualIp = req.get;
   console.log(
     `[${new Date().toLocaleString()}] ${req.ip} ${req.method} ${req.url}`
   );
@@ -15,6 +16,9 @@ app.get("/", (req, res) => {
   res.json({
     message: "Welcome to our API",
     cohort: process.env.LAMBDA_COHORT,
+    // this value is now extracted into our environment variable,
+    // so we can keep it out of our code and our repository
+    // secret: "hfaiwoehfo94032u4hnkljhbf"
     secret: process.env.SUPER_SECRET_API_KEY
   });
 });
